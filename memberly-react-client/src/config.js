fd2f069fe3de5f4939effbdacde26a5db57c5af1
unknown <guo362429@gmail.com>
2021-03-29 12:27:00 +0800

    fix bug

--- memberly-react-client/src/config.js
+++
@@ -1,8 +0,0 @@
-const currentUrl = document.URL.split("#")[0];
-module.exports = {
-  devHost: "http://localhost:3001",
-  prodHost:
-    currentUrl.indexOf("vip") > -1
-      ? "https://pay.960960.xyz"
-      : "https://coodopay.herokuapp.com",
-};
